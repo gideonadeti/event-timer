b81a219df225b29f3607f125393e904d1d4d566d
gideonadeti <gideonadeti0@gmail.com>
2024-11-12 04:38:09 +0000

    If event type is not countdown, reset date to yesterday

--- src/app/components/create-event.tsx
+++ src/app/components/create-event.tsx
@@ -101,7 +101,10 @@ function CreateEventForm({
       title: event?.title || "",
       description: event?.description || "",
       type: event?.type || "countdown",
-      date: addDays(new Date(), 1), // I tried using event?.date ?  new Date(event?.date) : new Date() but it doesn't work
+      date:
+        event?.type === "countdown"
+          ? addDays(new Date(), 1)
+          : subDays(new Date(), 1), // I tried using event?.date ?  new Date(event?.date) : new Date() but it doesn't work
       groupId:
         event?.groupId ||
         groups?.find((group) => group.name === "All")?.id ||
